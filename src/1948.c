9a1197fd7279841166c9f5e0528697fbcb5bd4e2
Jane Charlott <janechar@ev-b4.kzn.21-school.ru>
2023-03-09 19:29:42 +0300

    1

---
+++ src/1948.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include  <math.h>
+
+
+int  main() {
+        int  x, d;
+        d = scanf("%d", &x);
+            if (d == 1) {
+                  printf("%d")
+             }
+              else
+                   printf("n/a\n");
+    return 0;
+    }
+
+
